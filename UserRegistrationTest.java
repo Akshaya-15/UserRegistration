db0b24fc144d832e552ec89d7af61732dbca16f6
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-05 23:00:33 +0530

    test for passwordR2

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import java.lang.String;
 
 
-
+//@test
 public class UserRegistrationTest {
 	
 	private static final String UserResitation = null;
@@ -14,4 +14,32 @@ public class UserRegistrationTest {
 		UserRegistation obj =new UserRegistation();
 		assertTrue(Result);
 	}
+
+
+//@test
+public void TestlastName(boolean Result) {
+	UserRegistation obj =new UserRegistation();
+	assertTrue(Result);
+}
+
+
+//@test
+public void  Testemail(boolean Result) {
+	UserRegistation obj =new UserRegistation();
+	assertTrue(Result);
+}
+
+
+//@test
+public void  Testphonenum(boolean Result) {
+	UserRegistation obj =new UserRegistation();
+	assertTrue(Result);
+}
+
+//@test
+public void  TestpasswordR1(boolean Result) {
+	UserRegistation obj =new UserRegistation();
+	assertTrue(Result);
+
+}
 }

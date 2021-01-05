5cea9812c513525dc51472e7cdbceaf112605183
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-05 21:24:11 +0530

    test for firstname

---
+++ UserRegistrationTest.java
@@ -0,0 +1,17 @@
+package RegexPattern1;
+
+
+import static org.junit.Assert.*;
+import java.lang.String;
+
+
+
+public class UserRegistrationTest {
+	
+	private static final String UserResitation = null;
+ 
+	public void  TestfirstName(boolean Result) {
+		UserRegistation obj =new UserRegistation();
+		assertTrue(Result);
+	}
+}

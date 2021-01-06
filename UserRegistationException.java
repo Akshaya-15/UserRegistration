243cd2f05efd884b251073b8cb11c5cf6af18c42
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-06 14:53:26 +0530

    to method for exceptionhandling

---
+++ UserRegistationException.java
@@ -0,0 +1,10 @@
+package RegexPattern1;
+
+public class UserRegistationException extends Exception {
+
+	public UserRegistationException(String patternvalidity) {
+		super(patternvalidity);
+
+	}
+
+}
